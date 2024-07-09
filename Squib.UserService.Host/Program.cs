e414f6a704c4cde7ec472c3ac1031dee018b19f7
Nishant.Kumar <Nishant.Kumar@blockb.ca>
2024-07-08 23:59:23 -0500

    Database Add

--- Squib.UserService.Host/Program.cs
+++ Squib.UserService.Host/Program.cs
@@ -1,23 +1,31 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
-public class Program
+namespace Squib.UserService.Host
 {
-    public static void Main(string[] args)
+    public class Program
     {
-        CreateHostBuilder(args).Build().Run();
-    }
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureAppConfiguration((hostingContext, config) =>
-            {
-                config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                      .AddEnvironmentVariables();
-            })
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+
+            return Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder(args)
+                .UseSerilog() 
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder
+                        .ConfigureAppConfiguration((_, config) =>
+                        {
+                            config.AddJsonFile("appsettings.json", true, true);
+                        })
+                        .UseStartup<Startup>();
+                });
+        }
+    }
 }
